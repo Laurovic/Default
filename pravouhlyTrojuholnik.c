6bd5a5ab32578ffe79a9c839b9a4afb29c3f52af
Laurovič <94954906+Laurovic@users.noreply.github.com>
2021-11-28 16:54:11 +0100

    Update pravouhlyTrojuholnik.c

--- pravouhlyTrojuholnik.c
+++ pravouhlyTrojuholnik.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
     int str1, str2, str3;
 
     printf("Zadaj prvu stranu :");
     scanf("%i", &str1);
-
     printf("Zadaj druhu stranu :");
     scanf("%i", &str2);
-
     printf("Zadaj tretiu stranu :");
     scanf("%i", &str3);
 
@@ -17,12 +14,8 @@ int main(void)
     int tst2 = str2*str2==str3*str3+str1*str1;
     int tst3 = str3*str3==str2*str2+str1*str1;
 
-    if (tst1 || tst2 || tst3 == 1 )
-    {
+    if (tst1 || tst2 || tst3 == 1 ) {
         printf("true");
-    }
-    else
-    {
+    } else {
         printf("false");
-    }
-}
+    }}
